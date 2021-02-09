d8a03f4c9b0db35c5781074ee0f8832ebc25849c
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-09 15:17:23 +0100

    Sad path testing green

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -8,6 +8,17 @@ class Api::ArticlesController < ApplicationController
     article = Article.find(params['id'])
     render json: {article: article}
   end
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  
+  private
+ 
+  def record_not_found
+    
+    render json: {message: 'We cannot find the article you are looking for.'}, status: 404
+    
+  end
+
 end
 
 # Write a test with an it block. get 'path'
