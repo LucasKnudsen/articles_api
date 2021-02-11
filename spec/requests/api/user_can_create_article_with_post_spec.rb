d7f7b334c7d203b408cb807a1b3b7712617c09d8
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-11 14:36:15 +0100

    Sad path. Request should return 422 upon no specified title

--- spec/requests/api/user_can_create_article_with_post_spec.rb
+++ spec/requests/api/user_can_create_article_with_post_spec.rb
@@ -30,6 +30,21 @@ RSpec.describe 'POST /api/articles', types: :request do
   end
 
   describe 'unsuccessfully' do
-    
+
+    describe 'missing title' do
+      before do
+        post '/api/articles', params: {
+          article: {
+            
+            body: 'My Body'
+          }
+        }
+      end
+
+      it 'is expected to return a 422 status' do
+        binding.pry
+        expect(response).to have_http_status 422
+      end
+    end
   end
 end
